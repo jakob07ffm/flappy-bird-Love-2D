2823ec2ced1d817f0457a68a120d2c60184d029d
Jakob__init__ <jsuetfels@gmail.com>
2024-07-30 15:37:58 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,39 +1,27 @@
-
 local push = require 'push'
-
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
-
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
-
 local BIRD_WIDTH = 38
 local BIRD_HEIGHT = 24
-
-
-local PIPE_WIDTH = 70
-local PIPE_HEIGHT = 288
-local PIPE_SPEED = 60
-
-
-local PIPE_GAP = 90
-
-
-local background = love.graphics.newImage('background.png')
-local bird = love.graphics.newImage('bird.png')
-local pipe = love.graphics.newImage('pipe.png')
-
 local birdX = VIRTUAL_WIDTH / 2 - BIRD_WIDTH / 2
 local birdY = VIRTUAL_HEIGHT / 2 - BIRD_HEIGHT / 2
 local birdDY = 0
 
 
+local PIPE_WIDTH = 70
+local PIPE_HEIGHT = 288
+local PIPE_SPEED = 60
+local PIPE_GAP = 90
+
+
 local pipes = {}
 local spawnTimer = 0
-
 local score = 0
+
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -46,11 +34,18 @@ function love.load()
     })
 
     math.randomseed(os.time())
+
+    smallFont = love.graphics.newFont(8)
+    largeFont = love.graphics.newFont(16)
+    love.graphics.setFont(smallFont)
 end
+
 
 function love.keypressed(key)
     if key == 'space' then
         birdDY = -5
+    elseif key == 'r' then
+        resetGame()
     end
 end
 
@@ -58,9 +53,14 @@ function love.update(dt)
     birdDY = birdDY + 20 * dt
     birdY = birdY + birdDY
 
+    if birdY + BIRD_HEIGHT > VIRTUAL_HEIGHT or birdY < 0 then
+        resetGame()
+    end
+
     spawnTimer = spawnTimer + dt
     if spawnTimer > 2 then
-        table.insert(pipes, { x = VIRTUAL_WIDTH, y = math.random(PIPE_GAP, VIRTUAL_HEIGHT - PIPE_GAP) })
+        local pipeY = math.random(PIPE_GAP, VIRTUAL_HEIGHT - PIPE_GAP)
+        table.insert(pipes, { x = VIRTUAL_WIDTH, y = pipeY })
         spawnTimer = 0
     end
 
@@ -69,6 +69,13 @@ function love.update(dt)
 
         if pipes[i].x + PIPE_WIDTH < 0 then
             table.remove(pipes, i)
+            score = score + 1
+        end
+
+        if pipes[i].x < birdX + BIRD_WIDTH and birdX < pipes[i].x + PIPE_WIDTH then
+            if birdY < pipes[i].y - PIPE_GAP or birdY + BIRD_HEIGHT > pipes[i].y then
+                resetGame()
+            end
         end
     end
 end
@@ -76,14 +83,39 @@ end
 function love.draw()
     push:start()
 
-    love.graphics.draw(background, 0, 0)
 
-    love.graphics.draw(bird, birdX, birdY)
+    love.graphics.clear(0.5, 0.8, 1)  -- Sky blue background
 
+
+    love.graphics.setColor(1, 1, 0)  -- Yellow bird
+    love.graphics.rectangle('fill', birdX, birdY, BIRD_WIDTH, BIRD_HEIGHT)
+
+
+    love.graphics.setColor(0.4, 0.8, 0.4)  -- Green pipes
     for _, pipe in pairs(pipes) do
-        love.graphics.draw(pipe, pipe.x, pipe.y)
-        love.graphics.draw(pipe, pipe.x, pipe.y - PIPE_HEIGHT - PIPE_GAP)
+        love.graphics.rectangle('fill', pipe.x, pipe.y, PIPE_WIDTH, VIRTUAL_HEIGHT - pipe.y)
+        love.graphics.rectangle('fill', pipe.x, 0, PIPE_WIDTH, pipe.y - PIPE_GAP)
+    end
+
+
+    love.graphics.setFont(largeFont)
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.print('Score: ' .. tostring(score), 10, 10)
+
+
+    if birdY + BIRD_HEIGHT > VIRTUAL_HEIGHT or birdY < 0 then
+        love.graphics.setFont(largeFont)
+        love.graphics.printf('Game Over! Press R to Restart', 0, VIRTUAL_HEIGHT / 2 - 14, VIRTUAL_WIDTH, 'center')
     end
 
     push:finish()
+end
+
+
+function resetGame()
+    birdY = VIRTUAL_HEIGHT / 2 - BIRD_HEIGHT / 2
+    birdDY = 0
+    pipes = {}
+    spawnTimer = 0
+    score = 0
 end
